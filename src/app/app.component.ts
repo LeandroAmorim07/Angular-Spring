c1fa0bae4fe74c55ff977b62bda2a83b1bcc04cf
Leandro de  Souza Amorim <75571536+LeandroAmorim07@users.noreply.github.com>
2024-06-27 11:01:26 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [MatToolbarModule, RouterOutlet]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'crud-angular';
